b877be298c54337fd1e3db59a000ab11d5bb12bd
DanielCorso009 <dcorso1@student.fitchburgstate.edu>
2024-04-07 18:54:01 -0400

    leaderboard

---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import "$lib/app.css";
+    import { score } from "$lib/stores.js";
+    import { selectedCategories } from "$lib/stores.js"
+
+
+
+</script>
+<main>
+    <h1 class="text-lg text-black text-center">Leaderboard</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Rank</th>
+                <th>User Name</th>
+                <th>Score</th>
+                <th>Categories</th>
+            </tr>
+        </thead>
+        
+    </table>
+    <style>
+        h1 {
+            font-family: "Permanent Marker", cursive;
+            padding-bottom: 75px;
+        }
+        button{
+            font-family: "Permanent Marker", cursive;
+
+        }
+    </style>
+</main>
