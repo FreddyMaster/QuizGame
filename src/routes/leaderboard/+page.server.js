fa43be1c489ee88c9732aa29603395623fe0a5a6
DanielCorso009 <dcorso1@student.fitchburgstate.edu>
2024-04-08 01:55:30 -0400

    made the leaderboard today, grueling process

--- src/routes/leaderboard/+page.server.js
+++ src/routes/leaderboard/+page.server.js
@@ -1,47 +1,15 @@
 /** @type {import('./$types').PageServerLoad} */
 import { prisma } from "$lib/server/prisma"
 import { error, fail, redirect } from "@sveltejs/kit"
+import { score, selectedCategories } from "../../lib/stores.js"
 
-export const load = async ({ params }) => {
-    const question = await prisma.questions.findUnique({
-        where: {
-            question_id: Number(params.questionId),
-        },
-    })
-    if (!question) {
-        throw error(404, "Question not found")
+export const load = async () => {
+    const leaderboard = await prisma.leaderboard.findMany();
+    if (!leaderboard) {
+        throw error(404, "leaderboard ERR: No leaderboard could load :(")
     }
 
     return {
-        question,
+        leaderboard
     }
-}
-
-
-export const actions = {
-    updateQuestion: async ({ request, params }) => {
-        let { question, answer1, answer2, answer3, answer4, correctAnswer, category_id } = Object.fromEntries(await request.formData())
-        category_id = Number(category_id);
-
-        try {
-            await prisma.questions.update({
-                where: {
-                    question_id: Number(params.questionId),
-                },
-                data: {
-                    question,
-                    answer1,
-                    answer2,
-                    answer3,
-                    answer4, 
-                    correctAnswer,
-                    category_id
-                },
-            })
-        } catch (err) {
-            console.error(err)
-            return fail(500, { message: "Could not update question" })
-        }
-        throw redirect(303, '/admin');
-    },
 }
--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -1,19 +1,45 @@
 <script>
     import "$lib/app.css";
-    import { score } from "$lib/stores.js";
-    import { selectedCategories } from "$lib/stores.js"
+    import { score, selectedCategories } from "$lib/stores.js";
+    export let data;
+
+    $: ({ leaderboard } = data);
 </script>
 <main>
-    <h1 class="text-lg text-black text-center">Leaderboard</h1>
-    <table>
+    <h1 class="text-5xl text-black text-center">Leaderboard</h1>
+    <table class="table table-zebra border-solid bg-white">
         <thead>
             <tr>
                 <th>Rank</th>
                 <th>User Name</th>
                 <th>Score</th>
+                <th>Time</th>
                 <th>Categories</th>
             </tr>
         </thead>
+        {#each leaderboard as rank }
+        <tbody>
+            <tr>
+                <td>
+                    <div class="font-bold">
+                        {rank.rank}
+                    </div>
+                </td>
+                <td>
+                    {rank.username}
+                </td>
+                <td>
+                    {rank.score}
+                </td>
+                <td>
+                    {rank.time}
+                </td>
+                <td>
+                    {rank.categories}
+                </td>
+            </tr>
+        </tbody>
+    {/each}
         
     </table>
     <style>
@@ -21,10 +47,10 @@
             font-family: "Permanent Marker", cursive;
             padding-bottom: 75px;
         }
-        button{
+        thead{
             font-family: "Permanent Marker", cursive;
 
         }
     </style>
 </main>
-</script>
+
